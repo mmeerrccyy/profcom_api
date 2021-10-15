42fd770a3c3ed9989f47b9e3a0b511dfed4ea234
Taras Valakh <taras.mercury@gmail.com>
2021-10-15 19:33:22 +0300

    added heroku url to settings.py

--- profcom_api/settings.py
+++ profcom_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "https://profcom-test-api.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "profcom-test-api.herokuapp.com"]
 
 
 # Application definition
