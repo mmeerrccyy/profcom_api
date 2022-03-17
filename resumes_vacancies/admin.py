90e6d5c56fbe9d05290007b6d43ecba68131af5b
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-03-17 20:18:31 +0200

    added new models and fields in vacancy model

--- resumes_vacancies/admin.py
+++ resumes_vacancies/admin.py
@@ -9,5 +9,10 @@ class VacancyAdmin(admin.ModelAdmin):
     list_display = ('company_name', 'company_direction', 'vacancy_name', 'vacancy_salary', 'vacancy_date_added')
 
 
-admin.site.register(models.DirectionsModel)
 admin.site.register(models.VacancyModel, VacancyAdmin)
+
+tuple_models = (models.DirectionsModel, models.ResumeModel, models.WorkTimeModel, models.DegreeModel,
+                models.ExperienceModel, models.EnglishLevelModel)
+
+for model in tuple_models:
+    admin.site.register(model)
--- resumes_vacancies/urls.py
+++ resumes_vacancies/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('vacancies/', views.VacancyAPIView.as_view()),
     path('directions/', views.GetDirectionsAPIView.as_view()),
     path('work_times/', views.GetWorkTimeAPIView.as_view()),
+    path('degree/', views.GetDegreeAPIView.as_view()),
+    path('experience/', views.GetExperienceAPIView.as_view()),
+    path('english_level/', views.GetEnglishLevelAPIView.as_view()),
     path('resumes/', views.ResumeAPIView.as_view()),
     path('amount/', views.AmountVacancyResumeAPIView.as_view()),
 ]
