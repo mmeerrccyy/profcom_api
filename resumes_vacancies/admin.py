07b7a0dd42fb02b0a0924d12d260190b12cca8a7
Yaroslav Kovaliv <80165970+morento101@users.noreply.github.com>
2022-03-18 11:09:31 +0200

    Removed for loop
    That was a bad idea

--- resumes_vacancies/admin.py
+++ resumes_vacancies/admin.py
@@ -8,11 +8,11 @@ from . import models
 class VacancyAdmin(admin.ModelAdmin):
     list_display = ('company_name', 'company_direction', 'vacancy_name', 'vacancy_salary', 'vacancy_date_added')
 
-
+    
+admin.site.register(models.DirectionsModel)
 admin.site.register(models.VacancyModel, VacancyAdmin)
-
-tuple_models = (models.DirectionsModel, models.ResumeModel, models.WorkTimeModel, models.DegreeModel,
-                models.ExperienceModel, models.EnglishLevelModel)
-
-for model in tuple_models:
-    admin.site.register(model)
+admin.site.register(models.ResumeModel)
+admin.site.register(models.WorkTimeModel)
+admin.site.register(models.DegreeModel)
+admin.site.register(models.ExperienceModel)
+admin.site.register(models.EnglishLevelModel)
